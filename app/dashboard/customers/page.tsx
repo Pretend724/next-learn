4d9a24533bea1a37b2486269796a91052c34b803
Pretend <0724@zyk.com>
2025-11-24 22:30:31 +0800

    feat(customers):增加用户表

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,19 @@
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Customers",
+};
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
 
-export default function Page() {
-
-    return <p>Customers Page</p>;
+  return <Table customers={customers}></Table>;
 }
